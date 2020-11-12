db3cd73a9bddf0c009a877b634da4ed8ec2fe7ba
Franklyn Rodriguez <franklynr92@gmail.com>
2020-11-12 14:23:52 -0500

    adding prevent default

--- src/controllers/clipboard_controller.js
+++ src/controllers/clipboard_controller.js
@@ -1,6 +1,23 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+   
+
+    static targets = [ "source" ]
+    copy(event) {
+        event.preventDefault()
+        this.sourceTarget.select()
+        document.execCommand("copy")
+    }
+    connect() {
+        console.log("hello")
+        if (document.queryCommandSupported("copy")) {
+          this.element.classList.add("clipboard--supported")
+          }
+      }
+   
+}
+
     //stimulus loads the controller class, looks for a target name strings in a static array called targets.
     //for each targfet name in the array, stimulus adds three new properties to your controller
     //source target name becomes the following properties:
@@ -8,9 +25,4 @@ export default class extends Controller {
     //if there is no source target, accessing the property throws an error.
     ///this.sourcetargets evaluates to an array of all source targets in the controller's scope.
     //this.hasSourceTarget evaluates to true if there is a source target or falase if not.
-    static targets = [ "source" ]
-    copy() {
-        this.sourceTarget.select()
-        document.execCommand("copy")
-    }
-}
+  
